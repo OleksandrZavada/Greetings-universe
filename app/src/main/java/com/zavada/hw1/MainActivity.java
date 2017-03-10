695bb1fafc66764f4dc5a0b863c47352e18dea84
Oleksandr <oleksandr.zavada@gmail.com>
2017-03-10 23:29:08 +0200

    Initial commit

---
+++ app/src/main/java/com/zavada/hw1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.zavada.hw1;
+
+import android.content.Intent;
+import android.provider.SyncStateContract;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    Button btnActTwo;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                 Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
